85096113528f342a1d3e3e4dcf7d5e2c0c4d7c07
anhphha <74095848+anhphha@users.noreply.github.com>
2022-10-21 00:13:59 +0300

    application is working now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import { Icon } from 'react-icons-kit'
+import { search } from 'react-icons-kit/feather/search'
+import { WeatherViewer } from './Components/WeatherViewer'
 
 function App() {
+
+  // states
+  const [citySearch, setCitySearch] = useState('');
+  const [cityData, setCityData]=useState(null);
+
+  // city search form
+  const fetchCity = (e) =>{
+    e.preventDefault();
+    axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=DeehZZONl1Uh8AYYFH9uPwlEuKjC3oGv&q=${citySearch}`)
+    .then((res)=>{
+      setCityData(res.data[0]);
+      setCitySearch('');
+    }).catch(err=>console.log(err.message));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1 className="headline">React Weather API App</h1>
+      <h5 className="headline2">Made by Anh Ha</h5>
+      <form className='form-group custom-form' autoComplete='off'
+      onSubmit={fetchCity}>
+        <label>Search for a city to get weather data</label>
+        <div className='search-box'>
+          <input className='form-control' required placeholder='Enter city name...'
+          value={citySearch} onChange={(e)=>setCitySearch(e.target.value)}/>
+          <button type='submit' className="btn btn-secondary btn-sm">
+            <Icon icon={search} size={22}/>
+          </button>
+        </div>
+      </form>
+      {cityData&& <div style={{padding:10+'px', width: 100+'%'}}><WeatherViewer cityData={cityData}/></div>}
     </div>
   );
 }
